abc4034db22c92c56844aae4525069cef05a4631
lpoultersky <lpoultersky@gmail.com>
2023-01-25 15:18:19 +0000

    project init and db config

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
+  return (
+    <main>
+      <h1>Guestbook</h1>
+      <div>
+        {session ? (
+          <>
+            <p>hi {session.user?.name}</p>
+            <button
+              onClick={() => {
+                signOut().catch(console.log);
+              }}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => {
+              signIn("discord").catch(console.log);
+            }}
+          >
+            Login with Discord
+           </button>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
